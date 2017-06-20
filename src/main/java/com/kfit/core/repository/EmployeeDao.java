16eb740fa77add2b730574bc946e1b63568fbe8d
davi <hudawei@yeapoo.com>
2017-06-20 17:38:04 +0800

    jpa_test

--- src/main/java/com/kfit/core/repository/EmployeeDao.java
+++ src/main/java/com/kfit/core/repository/EmployeeDao.java
@@ -5,10 +5,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by davi on 2017/6/20.
  */
 public interface EmployeeDao extends JpaRepository<Employee,Long> {
     Page<Employee> findAll(Specification<Employee> specification, Pageable pageable);
+
+    @Query(nativeQuery = true, value = "select * from employee where name like %?1%")
+    List<Employee> queryByname(String name);
 }
--- src/test/java/com/kfit/SpringShiroTest.java
+++ src/test/java/com/kfit/SpringShiroTest.java
@@ -6,14 +6,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.*;
+import java.util.List;
 
 
 /**
@@ -29,17 +32,39 @@ public class SpringShiroTest {
 
     @Test
     public void test() throws Exception {
-        employeeDao.findAll(new Specification<Employee>() {
+        Page<Employee> employeePage = employeeDao.findAll(new Specification<Employee>() {
             @Override
             public Predicate toPredicate(Root<Employee> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
                 Path<Long> id = root.get("id");
                 Path<String> name = root.get("name");
                 root.join("department", JoinType.LEFT);
-                Predicate p1 = cb.and(cb.le(id, 10));
-                p1 = cb.and(p1, cb.like(name, "%" + "a" + "%"));
+                Predicate p1 = cb.le(id, 10);
+                Predicate p2 = cb.like(name, "%" + "a" + "%");
+                cb.and(p1, p2);
+//                criteriaQuery.where(p1, p2);//和上面等效
                 return null;
             }
         }, new PageRequest(0, 3, new Sort(Sort.Direction.DESC, "id")));
+        System.out.println(employeePage);
 
     }
+
+
+    @Test
+    public void test1() throws Exception {
+        List<Employee> employeePage = employeeDao.queryByname("a");
+        Assert.isInstanceOf(employeePage.getClass(), List.class);
+    }
+
+//    @Test
+//    public void test2() throws Exception {
+//        List<Map<String, Object>> employeePage = employeeDao.queryByname1("a");
+//        for (Map<String, Object> map : employeePage) {
+//            Map<String, Object> m = map;//此处会报错，jpa返回的是对象数组，不是map
+//            System.out.println(map);
+//        }
+//        Assert.isInstanceOf(employeePage.getClass(), List.class);
+//    }
+
+
 }
