13943d1bf7c77316031168569db23a8ed3f448a1
davi <hudawei@yeapoo.com>
2017-06-25 15:32:57 +0800

    映射枚举、嵌套映射

--- src/main/java/com/kfit/core/bean/User.java
+++ src/main/java/com/kfit/core/bean/User.java
@@ -1,6 +1,7 @@
 package com.kfit.core.bean;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by davi on 2017/6/25.
@@ -13,14 +14,17 @@ public class User {
     @GeneratedValue()
     private Long id;
 
-
     private Address homeAddress;
 
     @AttributeOverrides({@AttributeOverride(name = "street", column = @Column(name = "billing_street"))
             , @AttributeOverride(name = "city.zipCode", column = @Column(name = "billing_zipCode", length = 5))
-            , @AttributeOverride(name = "city.name", column = @Column(name = "billing_city",insertable=false ,updatable=false))
-})
+            , @AttributeOverride(name = "city.name", column = @Column(name = "billing_city", insertable = false, updatable = false))
+    })
     private Address billAddress;
+    @NotNull
+    @Column(columnDefinition = "varchar(20)")
+    @Enumerated(EnumType.STRING)
+    private ActionType actionType ;
 
     public Long getId() {
         return id;
@@ -45,4 +49,8 @@ public class User {
 //    public void setBillAddress(Address billAddress) {
 //        this.billAddress = billAddress;
 //    }
+
+    protected enum ActionType {
+        H, M, L
+    }
 }
